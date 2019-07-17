72a86a0c07b39a3c729418ecbfd5f2d625b98596
your username <dinak77240@gmail.com>
2019-07-17 17:01:51 +0200

    Add contact.ts to send form by email

--- front/src/app/contact/contact.component.ts
+++ front/src/app/contact/contact.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { SendEmailService } from 'src/app/send-email-service';
+import { Testimony } from 'src/app/testimony';
 
 @Component({
   selector: 'app-contact',
@@ -7,11 +9,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
-  public 
+  public buttonSend: string = "Submit";
 
-  constructor() { }
+  public name: string = null;
+  public email: string = null;
+  public comments: string = null;
+  public submitted: boolean = false;
+
+  @Output() public getDisplay: EventEmitter<any> = new EventEmitter()
+
+  constructor(private emailService: SendEmailService) { }
 
   ngOnInit() {
+    this.displayComponent = false;
+  }
+
+  onGetDisplay(event:boolean):void {
+    this.displayComponent = !event;
+    this.getDisplay.emit(this.displayComponent);
+  }
+
+  sendTestimony(event: boolean, testimonyFormIsValid: boolean): void {
+
+    if (event && testimonyFormIsValid) {
+      let testimony = new Testimony(
+        this.name, this.email, this.comments,
+      );
+      this.emailService.sendTestimonyEmail(
+        testimony
+        ).subscribe(
+          (response) => {
+            if (response) {
+              alert("Your message has been sent by email for validation");
+            } else {
+              alert("An error has occurred, your message has not been sent");
+            }
+          },
+          error => {
+            alert("An error has occurred, your message has not been sent");
+          }
+        );
+
+    } else if (event && (!testimonyFormIsValid)) {
+      this.submitted = true;
+    }
   }
 
 }
